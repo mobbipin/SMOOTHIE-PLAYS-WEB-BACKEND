0b4776169adfa5f7290da23b55ce4882388900ae
mobbipin <bipin@mobcreatives.com.np>
2025-02-14 13:07:49 +0545

    MAINTAINING API

--- src/index.js
+++ src/index.js
@@ -12,9 +12,14 @@ import { initializeSocket } from "./lib/socket.js";
 
 import { connectDB } from "./lib/db.js";
 import adminRoutes from "./routes/admin.route.js";
+import albumRoutes from "./routes/album.route.js";
 import authRoutes from "./routes/auth.route.js";
-import mobileAuthRoutes from "./routes/user.route.js";
-
+import songRoutes from "./routes/song.route.js";
+import statRoutes from "./routes/stat.route.js";
+import {
+  default as mobileAuthRoutes,
+  default as userRoutes,
+} from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -26,60 +31,67 @@ const httpServer = createServer(app);
 initializeSocket(httpServer);
 
 app.use(
-	cors({
-		origin: "http://localhost:3000",
-		credentials: true,
-	})
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
 );
 
 app.use(express.json()); // to parse req.body
 app.use(clerkMiddleware()); // this will add auth to req obj => req.auth
 app.use(
-	fileUpload({
-		useTempFiles: true,
-		tempFileDir: path.join(__dirname, "tmp"),
-		createParentPath: true,
-		limits: {
-			fileSize: 10 * 1024 * 1024, // 10MB  max file size
-		},
-	})
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "tmp"),
+    createParentPath: true,
+    limits: {
+      fileSize: 10 * 1024 * 1024, // 10MB  max file size
+    },
+  })
 );
 
 // cron jobs
 const tempDir = path.join(process.cwd(), "tmp");
 cron.schedule("0 * * * *", () => {
-	if (fs.existsSync(tempDir)) {
-		fs.readdir(tempDir, (err, files) => {
-			if (err) {
-				console.log("error", err);
-				return;
-			}
-			for (const file of files) {
-				fs.unlink(path.join(tempDir, file), (err) => {});
-			}
-		});
-	}
+  if (fs.existsSync(tempDir)) {
+    fs.readdir(tempDir, (err, files) => {
+      if (err) {
+        console.log("error", err);
+        return;
+      }
+      for (const file of files) {
+        fs.unlink(path.join(tempDir, file), (err) => {});
+      }
+    });
+  }
 });
 
 app.use("/api/auth", mobileAuthRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/auth", authRoutes);
-
+app.use("/api/songs", songRoutes);
+app.use("/api/albums", albumRoutes);
+app.use("/api/stats", statRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../frontend/dist")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
-	});
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 // error handler
 app.use((err, req, res, next) => {
-	res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : err.message });
+  res.status(500).json({
+    message:
+      process.env.NODE_ENV === "production"
+        ? "Internal server error"
+        : err.message,
+  });
 });
 
 httpServer.listen(PORT, () => {
-	console.log("Server is running on port " + PORT);
-	connectDB();
+  console.log("Server is running on port " + PORT);
+  connectDB();
 });
